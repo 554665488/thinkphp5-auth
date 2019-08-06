3ad26f1978804eec29e1fea567aa6e956918bde1
yfl <554665488@qq.com>
2019-08-07 00:10:00 +0800

    获取对象单例

--- src/config.php
+++ src/config.php
@@ -6,6 +6,6 @@ return [
         'auth_group'        => 'y_auth_group', //和Database/migrations 定义的迁移文件一致
         'auth_group_access' => 'y_auth_group_access', //和Database/migrations 定义的迁移文件一致
         'auth_rule'         => 'y_auth_rule', //和Database/migrations 定义的迁移文件一致
-        'auth_user'         => 'y_user', // 用户信息不带前缀表名
+        'auth_user'         => 'y_user', // 和Database/migrations 定义的迁移文件一致
     ],
 ];
